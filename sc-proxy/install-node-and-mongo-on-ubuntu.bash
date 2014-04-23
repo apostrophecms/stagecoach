148acc885178726d2dc08dcb55792bd05b557286
Tom Boutell <tom@punkave.com>
2014-04-23 10:13:56 -0400

    mongodb 2.6

--- sc-proxy/install-node-and-mongo-on-ubuntu.bash
+++ sc-proxy/install-node-and-mongo-on-ubuntu.bash
@@ -29,7 +29,8 @@ echo "Installing MongoDB" &&
 apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10 &&
 echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" >> /etc/apt/sources.list &&
 apt-get -y update &&
-apt-get -y install mongodb-10gen &&
+# Install MongoDB 2.6, not the old 2.4 installed by mongodb-10gen
+apt-get install -y mongodb-org &&
 echo "Installed MongoDB" &&
 echo "Configuring MongoDB to listen only on localhost" &&
 echo "bind_ip = 127.0.0.1" >> /etc/mongodb.conf &&
